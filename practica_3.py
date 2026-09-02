ef8299fcb0d8d75cb0443162d812d3b965351271
iriosd-pixel <iriosd@unemi.edu.ec>
2026-09-01 22:26:09 -0500

    primer commit

--- practica_3.py
+++ practica_3.py
@@ -17,3 +17,17 @@ else:
         contador=contador+1
         valores=valores//10
 print(f"En la cifra hay {contador} numeros")
+
+#================================================
+# 2) Lee N números y muestra la suma de los pares y la suma de los impares por separado.
+#================================================
+cantidad=int(input("Ingrese la cantidad de numeros: "))
+suma_par=0
+suma_impar=0
+for i in range(cantidad):
+    valor=int(input("Ingrese un numero: "))
+    if valor%2==0:
+        suma_par=suma_par+valor
+    else:
+        suma_impar=suma_impar+valor
+print(f"La suma par es: {suma_par} y la suma impar es: {suma_impar}")
